d7146c620eb35fb50dfd8a7d15327402b57059ac
Mattias Michaux <mattias.michaux@gmail.com>
2015-03-21 13:59:51 +0100

    Added working code to search for isbn neighbour

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,15 +11,67 @@
 
 angular.module('boekbuurApp')
   .controller('MainCtrl', function ($scope, DataSource) {
-    var setData = function(data) {
-        $scope.json = data;
+
+    var prev = false;
+    var next = false;
+    var prev_isbn = false;
+    var next_isbn = false;
+
+    var setDataPrev = function(data) {
+      if (data.aquabrowser.meta.count == "1") {
+        prev = data;
+        console.log(prev);
+      }
+      else {
+        prev_isbn--;
+        var request = {
+          isbn : prev_isbn,
+          callback : setDataPrev
+        }
+        DataSource.get(request);
+      }
+    }
+
+    var setDataNext = function(data) {
+      if (data.aquabrowser.meta.count == "1") {
+        next = data;
+        console.log(next);
+      }
+      else {
+        next_isbn++;
+        var request = {
+          isbn : next_isbn,
+          callback : setDataNext
+        }
+        DataSource.get(request);
+      }
     }
 
     $scope.zoek = function () {
-      var request = {
-        isbn : $scope.isbnWaarde,
-        callback : setData
-      }
-      DataSource.get(request);
+      // prev = false;
+      // next = false;
+      // prev_isbn = $scope.isbnWaarde - 1;
+      // next_isbn = 1 + parseInt($scope.isbnWaarde);
+
+      // var array = ['a', 'b'];
+
+      // while(array.indexOf(prev_isbn) = -1) {
+      //   prev_isbn--;
+      // }
+
+      // var request = {
+      //   isbn : prev_isbn,
+      //   callback : setDataPrev
+      // }
+      // DataSource.get(request);
+
+      // var request = {
+      //   isbn : next_isbn,
+      //   callback : setDataNext
+      // }
+      // DataSource.get(request);
+
+
+
     };
 });

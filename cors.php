930603f95d3480f62ef58e37260b4d18d051e931
Mattias Michaux <mattias.michaux@gmail.com>
2015-03-21 12:37:20 +0100

    Added origin header.

--- cors.php
+++ cors.php
@@ -1,4 +1,6 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+
 $isbn = $_GET['isbn'];
 $a = file_get_contents("http://zoeken.bibliotheek.be/api/v0/search/?q=isbn:" . $isbn . "&authorization=ac135e89f84460a251a6283a14180a22");
 echo $a;
